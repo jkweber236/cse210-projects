c3423ea0e72b87d2261f1929837466cb97b22c5e
jkweber236 <jkweber236@gmail.com>
2023-09-14 15:08:27 -0600

    Completed C# Prep 2

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,43 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your grade percentage? ");
+        string gradePercentage = Console.ReadLine();
+        int gradeNumber = int.Parse(gradePercentage);
+
+        string letter = "";
+
+        if (gradeNumber >= 90)
+        {
+            letter = "A";
+        }
+        else if (gradeNumber >= 80)
+        {
+            letter = "B";
+        }
+        else if (gradeNumber >= 70)
+        {
+            letter = "C";
+        }
+        else if (gradeNumber >= 60)
+        {
+            letter = "D";
+        }
+        else if (gradeNumber < 60)
+        {
+            letter = "F";
+        }
+
+        Console.WriteLine($"Your grade is: {letter}");
+
+        if (gradeNumber >= 70)
+        {
+            Console.WriteLine("Congratulations! You passed the course.");
+        }
+        else
+        {
+            Console.WriteLine("Better luck next time.");
+        }
+
     }
 }
